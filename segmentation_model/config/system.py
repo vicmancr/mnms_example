6e649d1abd8e995a253aa7d08fd0072c08a89929
Victor M. Campello <victor.campello@ub.edu>
2020-09-14 13:51:29 +0200

    Change in project root.

--- segmentation_model/config/system.py
+++ segmentation_model/config/system.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 home = os.environ.get('HOME')
 project_root = os.path.join(home, 'mnms_example')
 if os.environ.get('SINGULARITY_NAME') is not None:
-    project_root = os.path.join('/mnms')
+    project_root = os.path.join('/gpfs/projects/bsc39/bsc39304/mnms')
 
 data_base = os.path.join(project_root, 'data')
 log_root = os.path.join(project_root, 'models')
