8f7053eea0dddc434d58ec04ce87b3a874382fff
Victor M. Campello <victor.campello@ub.edu>
2020-09-14 13:50:57 +0200

    Baseline models jobs.

---
+++ jobs/02-unet-rot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="unet-rot"
+#SBATCH -n 1 # Number of cores
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -t 0-48:00 # Runtime: 24 hours
+#SBATCH -p high # Partition to submit to
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12G
+#SBATCH -o /homedtic/vcampello/logs/unet-rot-%j.out # File to which STDOUT will be written
+#SBATCH -e /homedtic/vcampello/logs/unet-rot-%j.err # File to which STDERR will be written
+
+# Dependencies
+module load SINGULARITY/3.5.2
+
+cd /home/bsc39/bsc39304/mnms/
+singularity exec --nv images/pollito.sif python train.py unet2D_bn_modified_wxent -d mnms -b 8 -r -n rot
